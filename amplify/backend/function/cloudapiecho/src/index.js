5970ae7c2a25d5ac61db4b1f6921d06d3e9484d0
Evgeny Vinnik <evgenyvinnik@ya.ru>
2022-05-12 10:51:15 -0700

    implement echo bot

--- amplify/backend/function/cloudapiecho/src/index.js
+++ amplify/backend/function/cloudapiecho/src/index.js
@@ -4,79 +4,125 @@
 Amplify Params - DO NOT EDIT */
 
 const aws = require('aws-sdk');
-const axios = require('axios').default;
+const https = require("https");
 
-exports.handler = async (event, context, callback)  => {
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  let httpMethod;
-
-  if (event.context !== undefined) {
-    httpMethod = event.context["http-method"];
-  } else {
-    //used to test with lambda-local
-    httpMethod = "PUT";
-  }
-
-  if (httpMethod === "GET") {
-    const queryParams = event.params.querystring;
-    const mode = queryParams["hub.mode"];
-    if (mode === "subscribe") {
-      const { Parameters } = await (new aws.SSM())
-        .getParameters({
-          Names: ["verify_token"].map(secretName => process.env[secretName]),
-          WithDecryption: true,
-        })
-        .promise();
-      const VERIFY_TOKEN = Parameters.pop().Value;
-      const verifyToken = queryParams["hub.verify_token"];
-      if (verifyToken === VERIFY_TOKEN) {
-        var challenge = queryParams["hub.challenge"];
-        callback(null, parseInt(challenge));
+exports.handler = async (event)  => {
+  let response;
+  if (event.httpMethod === "GET") {
+    let queryParams = event.multiValueQueryStringParameters;
+    if (queryParams != null) {
+      const mode = queryParams["hub.mode"];
+      if (mode == "subscribe") {
+        const { Parameters } = await (new aws.SSM())
+          .getParameters({
+            Names: ["verify_token"].map(secretName => process.env[secretName]),
+            WithDecryption: true,
+          })
+          .promise();
+        const VERIFY_TOKEN = Parameters.pop().Value;
+        const verifyToken = queryParams["hub.verify_token"];
+        if (verifyToken == VERIFY_TOKEN) {
+          let challenge = queryParams["hub.challenge"];
+          response = {
+              "statusCode": 200,
+              "body": parseInt(challenge),
+              "isBase64Encoded": false
+          };
+        } else {
+          const responseBody = "Error, wrong validation token";
+          response = {
+              "statusCode": 403,
+              "body": JSON.stringify(responseBody),
+              "isBase64Encoded": false
+          };
+        }
       } else {
-        callback(null, "Error, wrong validation token");
+          const responseBody = "Error, wrong mode";
+          response = {
+              "statusCode": 403,
+              "body": JSON.stringify(responseBody),
+              "isBase64Encoded": false
+        };
       }
-    } else {
-      callback(null, "Error, wrong mode");
     }
-  } else {
+    else {
+      const responseBody = "Error, no query parameters";
+      response = {
+          "statusCode": 403,
+          "body": JSON.stringify(responseBody),
+          "isBase64Encoded": false
+      };
+    }
+  } else if (event.httpMethod === "POST") {
     // process POST request (WhatsApp chat messages)
-    let entries = event["body-json"].entry;
+    let body = JSON.parse(event.body)
+    let entries = body.entry;
     for (let entry of entries) {
       for (let change of entry.changes) {
         let value = change.value;
-        let phone_number_id = value.metadata.phone_number_id;
-        for (let message of value.messages) {
-          if (message.type === 'text') {
-            const { Parameters } = await (new aws.SSM())
-              .getParameters({
-                Names: ["whatsapptoken"].map(secretName => process.env[secretName]),
-                WithDecryption: true,
-              })
-              .promise();
-            const WHATSAPP_TOKEN = Parameters.pop().Value;
-            let from = message.from;
-            let message_body = message.text.body;
-            axios({
-              method: "POST",
-              url:
-                "https://graph.facebook.com/v12.0/" +
-                phone_number_id +
-                "/messages?access_token=" +
-                WHATSAPP_TOKEN,
-              data: {
-                messaging_product: "whatsapp",
-                to: from,
-                text: { body: "Ack from AWS lambda: " + message_body },
-              },
-              headers: { "Content-Type": "application/json" },
-            });
-            callback(null, "Done");
+        if(value != null) {
+          let phone_number_id = value.metadata.phone_number_id;
+          if (value.messages != null) {
+            for (let message of value.messages) {
+              if (message.type === 'text') {
+                const { Parameters } = await (new aws.SSM())
+                  .getParameters({
+                    Names: ["whatsapptoken"].map(secretName => process.env[secretName]),
+                    WithDecryption: true,
+                  })
+                  .promise();
+                const WHATSAPP_TOKEN = Parameters.pop().Value;
+                let from = message.from;
+                let message_body = message.text.body;
+                let reply_message = "Ack from AWS lambda: " + message_body;
+                sendReply(phone_number_id, WHATSAPP_TOKEN, from, reply_message);
+                const responseBody = "Done";
+                response = {
+                    "statusCode": 200,
+                    "body": JSON.stringify(responseBody),
+                    "isBase64Encoded": false
+                };
+              }
+            }
           }
         }
       }
     }
-    
-    callback(null, event);
+  } else {
+    const responseBody = "Unsupported method";
+    response = {
+        "statusCode": 403,
+        "body": JSON.stringify(responseBody),
+        "isBase64Encoded": false
+    };
   }
+  return response;
+}
+
+const sendReply = (phone_number_id, whatsapp_token, to, reply_message) => {
+  let json = {
+    messaging_product: "whatsapp",
+    to: to,
+    text: { body: reply_message },
+  };
+  let data = JSON.stringify(json);
+  let path = "/v12.0/"+phone_number_id+"/messages?access_token="+whatsapp_token;
+  let options = {
+    host: "graph.facebook.com",
+    path: path,
+    method: "POST",
+    headers: { "Content-Type": "application/json" }
+  };
+  let callback = (response) => {
+    let str = "";
+    response.on("data", (chunk) => {
+      str += chunk;
+    });
+    response.on("end", () => {
+    });
+  };
+  let req = https.request(options, callback);
+  req.on("error", (e) => {});
+  req.write(data);
+  req.end();
 }
